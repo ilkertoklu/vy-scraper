e28227607f8ffee9b2b616f1342a9bec24a49a24
İlker Toklu <ilkertoklu@protonmail.com>
2022-07-19 14:33:23 +0300

    initial commit

--- scraper.rb
+++ scraper.rb
@@ -2,6 +2,7 @@
 
 require 'httparty'
 require 'nokogiri'
+require 'spreadsheet'
 
 city = 'Samsun'
 
@@ -11,7 +12,48 @@ def request(adress)
   doc = Nokogiri::HTML(html)
 end
 
-def listing(link)
+def type(city)
+  lastpage = request("https://vymaps.com/TR/#{city}").xpath('//b[1]').text.split(' ')[-2].to_i + 1  
+  types = []
+  page = 1
+
+  while page < lastpage
+    url = "https://vymaps.com/TR/#{city}/#{page}"
+    data = request(url).xpath('//div/a/@href')
+    types += data
+    page += 1
+  end
+  link(types)
+end
+
+def link(types)
+  book = Spreadsheet::Workbook.new
+  book.create_worksheet :name => "Places"
+  sheet = book.worksheet(0)
+  headers = 'Name','Type','Adress','Coordinate','Parking','Rating','Phone','Mail','Social','Website','Open-Hours' 
+  sheet.row(0).concat headers
+  index = 1
+
+  types.each do |type|
+    lastpage = request(type).xpath('//div/b[1]').text.split(' ')[-2].to_i + 1
+    book.write 'deneme.xls'
+    page = 1
+
+    while page < lastpage
+      link_with_page = type.text + page.to_s
+      doc = request(link_with_page)
+      links = doc.xpath('//p/b/a/@href')
+      page += 1
+
+      links.each do |link|
+        listing(link, book, sheet, index)
+        index += 1
+      end
+    end
+  end
+end
+
+def listing(link, book, sheet, index)
   doc = request(link)
   name = doc.xpath('//h1/a/b').text
   type = doc.xpath('//tr[1]/td[3]').text
@@ -28,40 +70,8 @@ def listing(link)
   ophours = doc.xpath("//div[@class='five columns']/span").text.split(/(?=[A-Z])/)
   ophours = ophours.join(', ')
 
-  list = "#{name}\n#{type}\n#{adress}\n#{coordinate}\n#{parking}\n#{rating}\n#{phone}\n#{mail}\n#{social}\n#{website}\n#{ophours}\n\n"  
-end
-
-def type(city)
-  types = []
-  page = 1
-
-  lastpage = request("https://vymaps.com/TR/#{city}").xpath('//b[1]').text.split(' ')[-2].to_i + 1
-  puts "Looking for place types...\n"
-
-  while page < lastpage
-    url = "https://vymaps.com/TR/#{city}/#{page}"
-    data = request(url).xpath('//div/a/@href')
-    types += data
-    page += 1
-  end
-  link(types)
-end
-
-def link(types)
-  types.each do |type|
-    lastpage = request(type).xpath('//div/b[1]').text.split(' ')[-2].to_i + 1
-    page = 1
-
-    while page < lastpage
-      link_with_page = type.text + page.to_s
-      doc = request(link_with_page)
-      links = doc.xpath('//p/b/a/@href')
-      page += 1
-
-      links.each do |link|
-        puts listing(link)
-      end
-    end
-  end
+  list = name, type, adress, coordinate, parking, rating, phone, mail, social, website, ophours
+  sheet.row(index).concat list
+  puts index
 end
 type(city)
