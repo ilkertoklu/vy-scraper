95271662aad7394b98160b5de6f9001314cd47c9
İlker Toklu <ilkertoklu@protonmail.com>
2022-08-04 10:55:13 +0300

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -4,52 +4,53 @@ require 'httparty'
 require 'nokogiri'
 require 'spreadsheet'
 
-city = 'Samsun' # Change as where you wish to scrap, or request the cities and countries array.
+city = 'Samsun'
 
-def request(adress)
-  response = HTTParty.get(adress)
+def request_address(address)
+  response = HTTParty.get(address)
   html = response.body
-  Nokogiri::HTML(html)
+  Nokogiri::HTML(html) # => Nokogiri::XML
 end
 
 def encoder(list)
-  list.map do |info|
-    info.encode('UTF-16le', invalid: :replace, replace: '').encode('UTF-8') unless info.nil? || info.empty?
-  end
+  list.map { |info| info&.encode('UTF-16le', invalid: :replace, replace: '')&.encode('UTF-8') }
 end
 
 def column_check(db)
   init_headers = [:name, :"place types", :address, :coordinate, :phone,
                   :mail, :parking, :rating, :social, :website, :ophours]
-  data = []
-  init_headers.each { |header| data << db[header] }
-  data
+  init_headers.map { |header| db[header] }
 end
 
 def database(link)
-  doc = request(link)
+  doc = request_address(link)
   table = doc.xpath('//tbody/tr').map(&:text)
-  headers = [:name] + table.map { |row| row[0, row.index(':')].downcase.to_sym } + [:ophours]
-
-  content = table.map { |row| row[row.index(':') + 1, row.length] }
-  ophours = [doc.xpath("//div[@class='five columns']/span").text.split(/(?=[A-Z])/).join(', ')]
-  body = [doc.xpath('//h1/a/b').text] + content + ophours
-  Hash[headers.zip body]
+  Hash[header(table).zip body(doc, table)]
 end
 
-def collect_types(city)
-  last_page = request("https://vymaps.com/TR/#{city}").xpath('//b[1]').text.split(' ')[-2].to_i + 1
+def header(table)
+  [:name] + table.map { |row| row[0, row.index(':')].downcase.to_sym } + [:ophours]
+end
+
+def body(doc, table)
+  content = table.map { |row| row[row.index(':') + 1, row.length] }
+  ophours = [doc.xpath("//div[@class='five columns']/span").text.split(/(?=[A-Z])/).join(', ')]
+  [doc.xpath('//h1/a/b').text] + content + ophours
+end
+
+def place_types(city)
+  last_page = request_address("https://vymaps.com/TR/#{city}").xpath('//b[1]').text.split(' ')[-2].to_i + 1
   page = 1
   types = []
 
   while page < last_page
-    types += request("https://vymaps.com/TR/#{city}/#{page}").xpath('//div/a/@href')
+    types += request_address("https://vymaps.com/TR/#{city}/#{page}").xpath('//div/a/@href')
     page += 1
   end
   types
 end
 
-def book_builder
+def build_book
   workbook = Spreadsheet::Workbook.new
   sheet = workbook.create_worksheet name: 'places'
   sheet.row(0).concat %w[name type address coordinate phone mail parking rating social website ophours]
@@ -58,28 +59,28 @@ def book_builder
   { workbook: workbook, sheet: sheet, index: index }
 end
 
-def link(city)
-  book = book_builder
-  collect_types(city).each do |type|
-    last_page = request(type).xpath('//div/b[1]').text.split(' ')[-2].to_i + 1
+def linker(city)
+  book = build_book
+  place_types(city).each do |type|
+    last_page = request_address(type).xpath('//div/b[1]').text.split(' ')[-2].to_i + 1
     page = 1
-    collect_urls(page, last_page, type, book)
+    url_stack(page, last_page, type, book)
   end
 end
 
-def collect_urls(page, last_page, type, book)
+def url_stack(page, last_page, type, book)
   while page < last_page
-    links = request(type.text + page.to_s).xpath('//p/b/a/@href')
+    links = request_address(type.text + page.to_s).xpath('//p/b/a/@href')
     book[:workbook].write 'places_samsun-deneme-pg.xls'
-    append(links, book)
+    append_to_row(links, book)
     page += 1
   end
 end
 
-def append(links, book)
+def append_to_row(links, book)
   links.each do |link|
     book[:sheet].row(book[:index]).concat encoder(column_check(database(link)))
     p book[:index] += 1
   end
 end
-link(city)
+linker(city)
